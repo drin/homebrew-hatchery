c83d88f55c5ecd6d690a2d87186ede1ce8fc32bc
Aldrin Montana <octalene.dev@pm.me>
2024-08-21 15:52:11 -0700

    task: adding recipe for mohair-substrait

---
+++ Formula/mohair-substrait.rb
@@ -0,0 +1,25 @@
+class MohairSubstrait < Formula
+  desc     "Shared library to interface with Substrait and Mohair protocols"
+  homepage "https://github.com/drin/mohair-substrait.git"
+  url      "https://github.com/drin/mohair-substrait.git",
+    tag: "v0.1.0",
+    commit: "8057238d22a0ca980d5a040f6c49c02d856f1b1e"
+  license "Apache-2.0"
+
+  depends_on "meson"  => :build
+  depends_on "ninja"  => :build
+
+  def install
+    # Get all submodules
+    system 'git', 'submodule', 'init'
+    system 'git', 'submodule', 'update', '--recursive', '--remote'
+
+    # Build artifacts
+    build_dpath = 'build-dir-release'
+
+    # Build and install the code
+    system 'meson', 'setup'  , build_dpath
+    system 'meson', 'compile', '-C', build_dpath
+    system 'meson', 'install', '-C', build_dpath
+  end
+end
