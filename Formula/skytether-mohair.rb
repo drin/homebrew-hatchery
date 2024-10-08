232116f19d4356b93cb878ad737a98a1e8f23c9a
Aldrin Montana <octalene.dev@pm.me>
2024-10-08 10:31:41 -0700

    task: updated skytether-mohair to v0.3.0
    This version should actually work with the mohair-substrait dependency

--- Formula/skytether-mohair.rb
+++ Formula/skytether-mohair.rb
@@ -2,13 +2,15 @@ class SkytetherMohair < Formula
   desc     "Mohair is the query processing layer of Skytether for cooperative query decomposition"
   homepage "https://research.aldrinmontana.com/"
   url      "https://github.com/drin/mohair.git",
-    tag: "v0.2.0",
-    revision: "54fd307a510d11d90f2575bddc48c6a8e3af6467"
+    tag: "v0.3.0",
+    revision: "40bccb6a0ec8004c0ee3b7b4fafe963d62fed4b6"
   license  "Apache-2.0"
 
   depends_on "meson" => :build
   depends_on "cmake" => :build
   depends_on "ninja" => :build
+
+  depends_on "mohair-substrait" => :build
 
   def install
     build_dpath = 'build-dir'
