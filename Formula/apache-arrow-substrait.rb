cd9c5a800ed39fd72b4ff654c2331620fe428284
Aldrin Montana <octalene.dev@pm.me>
2024-11-13 09:46:31 -0800

    update(apache-arrow-substrait): 17.0.0 -> 18.0.0
    Changelog: https://arrow.apache.org/release/18.0.0.html

--- Formula/apache-arrow-substrait.rb
+++ Formula/apache-arrow-substrait.rb
@@ -1,11 +1,11 @@
 class ApacheArrowSubstrait < Formula
   desc     "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
-  url      "https://dlcdn.apache.org/arrow/arrow-17.0.0/apache-arrow-17.0.0.tar.gz"
-  mirror   "https://archive.apache.org/dist/arrow/arrow-17.0.0/apache-arrow-17.0.0.tar.gz"
-  sha256   "9d280d8042e7cf526f8c28d170d93bfab65e50f94569f6a790982a878d8d898d"
+  url      "https://dlcdn.apache.org/arrow/arrow-18.0.0/apache-arrow-18.0.0.tar.gz"
+  mirror   "https://archive.apache.org/dist/arrow/arrow-18.0.0/apache-arrow-18.0.0.tar.gz"
+  sha256   "abcf1934cd0cdddd33664e9f2d9a251d6c55239d1122ad0ed223b13a583c82a9"
   license  "Apache-2.0"
-  head     "https://github.com/apache/arrow.git", branch: "apache-arrow-17.0.0"
+  head     "https://github.com/apache/arrow.git", branch: "apache-arrow-18.0.0"
   revision 1
 
   depends_on "boost"           => :build
@@ -56,7 +56,7 @@ class ApacheArrowSubstrait < Formula
       -DARROW_ORC=OFF
       -DARROW_PARQUET=ON
       -DARROW_PROTOBUF_USE_SHARED=OFF
-      -DARROW_PROTOBUF_BUILD_VERSION=v28.2
+      -DARROW_PROTOBUF_BUILD_VERSION=v28.3
       -DARROW_S3=ON
       -DARROW_SUBSTRAIT=ON
       -DARROW_WITH_BZ2=ON
