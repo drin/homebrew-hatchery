f32d7b1cc96df2ebadb15037e4e3216ec7757398
Aldrin Montana <octalene.dev@pm.me>
2025-02-19 11:08:42 -0800

    formula(grpc-static): updated to v1.70.1
    I didn't update this formula after updating abseil and protobuf, and I
    think on linux it's an issue

--- Formula/grpc-static.rb
+++ Formula/grpc-static.rb
@@ -2,20 +2,10 @@ class GrpcStatic < Formula
   desc "Next generation open source RPC library and framework"
   homepage "https://grpc.io/"
   license "Apache-2.0"
-  revision 6
+  url "https://github.com/grpc/grpc.git",
+      tag:      "v1.70.1",
+      revision: "5e099002c1600c580ebe1e6741f8ff8b182ffea4"
   head "https://github.com/grpc/grpc.git", branch: "master"
-
-  stable do
-    url "https://github.com/grpc/grpc.git",
-        tag:      "v1.62.2",
-        revision: "96f984744fe728e196c11d33b91b022566c0d40f"
-
-    # Backport fix for Protobuf 26
-    patch do
-      url "https://github.com/grpc/grpc/commit/98a96c5068da14ed29d70ca23818b5f408a2e7b4.patch?full_index=1"
-      sha256 "5c4fc4307d0943ce3c9a07921bddaa24ca3d504adf38c9b0f071e23327661ac1"
-    end
-  end
 
   # There can be a notable gap between when a version is tagged and a
   # corresponding release is created, so we check releases instead of the Git
@@ -39,9 +29,9 @@ class GrpcStatic < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "cmake" => :build
-  depends_on "libtool" => :build
-  depends_on "pkg-config" => :test
+  depends_on "cmake"    => :build
+  depends_on "libtool"  => :build
+  depends_on "pkgconf"  => :test
   depends_on "abseil-static"
   depends_on "c-ares"
   depends_on "openssl@3"
@@ -62,9 +52,8 @@ class GrpcStatic < Formula
     cause "Requires C++17 features not yet implemented"
   end
 
-  fails_with gcc: "5" # C++17
-
   def install
+    #ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1100)
     ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1100)
     args = %W[
       -DCMAKE_CXX_STANDARD=17
@@ -79,30 +68,47 @@ class GrpcStatic < Formula
       -DgRPC_ZLIB_PROVIDER=package
       -DgRPC_RE2_PROVIDER=package
     ]
-    system "cmake", "-S", ".", "-B", "_build", *args, *std_cmake_args
-    system "cmake", "--build", "_build"
-    system "cmake", "--install", "_build"
+
+    linker_flags = []
+    linker_flags += %w[-undefined dynamic_lookup] if OS.mac?
+
+    args << "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,#{linker_flags.join(",")}" if linker_flags.present?
+
+    build_dir = "_build"
+    system "cmake", "-S", ".", "-B", build_dir, *args, *std_cmake_args
+    system "cmake",       "--build", build_dir
+    system "cmake",     "--install", build_dir
 
     # The following are installed manually, so need to use CMAKE_*_LINKER_FLAGS
-    args = %W[
-      -DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath,#{rpath}
+    linker_flags += %W[-rpath #{rpath} -rpath #{rpath(target: HOMEBREW_PREFIX/"lib")}]
+    cli_args = %W[
+      -DCMAKE_EXE_LINKER_FLAGS=-Wl,#{linker_flags.join(",")}
       -DgRPC_BUILD_TESTS=ON
+      -DgRPC_ABSL_PROVIDER=package
+      -DgRPC_CARES_PROVIDER=package
+      -DgRPC_PROTOBUF_PROVIDER=package
+      -DgRPC_SSL_PROVIDER=package
+      -DgRPC_ZLIB_PROVIDER=package
+      -DgRPC_RE2_PROVIDER=package
     ]
-    system "cmake", "-S", ".", "-B", "_build", *args, *std_cmake_args
-    system "cmake", "--build", "_build", "--target", "grpc_cli"
-    bin.install "_build/grpc_cli"
-    lib.install Dir["_build/#{shared_library("libgrpc++_test_config", "*")}"]
+
+    cli_build_dir = "_cli_build"
+    system "cmake", "-S", ".", "-B", cli_build_dir, *cli_args, *std_cmake_args
+    system "cmake",       "--build", cli_build_dir, "--target", "grpc_cli"
+
+    bin.install "#{build_dir}/grpc_cli"
+    lib.install Dir["#{build_dir}/#{shared_library("libgrpc++_test_config", "*")}"]
   end
 
   test do
-    (testpath/"test.cpp").write <<~EOS
+    (testpath/"test.cpp").write <<~CPP
       #include <grpc/grpc.h>
       int main() {
         grpc_init();
         grpc_shutdown();
         return GRPC_STATUS_OK;
       }
-    EOS
+    CPP
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["openssl@3"].opt_lib/"pkgconfig"
     pkg_config_flags = shell_output("pkg-config --cflags --libs libcares protobuf re2 grpc++").chomp.split
     system ENV.cc, "test.cpp", "-L#{Formula["abseil-static"].opt_lib}", *pkg_config_flags, "-o", "test"
