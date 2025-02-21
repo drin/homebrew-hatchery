ba11be7acbff1cfa89a896262dee0204148c9c8e
Aldrin Montana <octalene.dev@pm.me>
2025-02-21 13:00:06 -0800

    formula(apache-arrow-substrait): minor update
    Not sure what this change does, presumably it updates an expected
    failure mode. Propagating the change from the "apache-arrow" formula.

--- Formula/apache-arrow-substrait.rb
+++ Formula/apache-arrow-substrait.rb
@@ -32,7 +32,10 @@ class ApacheArrowSubstrait < Formula
   uses_from_macos "bzip2"
   uses_from_macos "zlib"
 
-  fails_with gcc: "5"
+  fails_with gcc: do
+    version "12"
+    cause "Protobuf 29+ generated code with visibility and deprecated attributes needs GCC 13+"
+  end
 
   def install
     ENV.llvm_clang if OS.linux?
